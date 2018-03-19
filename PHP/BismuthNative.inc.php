dbf3cafd099583a7428f11f2ea68a1f6f7bd0f77
actuino <ecrire@actuino.com>
2018-03-19 19:16:04 +0100

    Working php client lib

--- PHP/BismuthNative.inc.php
+++ PHP/BismuthNative.inc.php
@@ -7,40 +7,152 @@
  * @package BismuthAPI
  * @author @EggPool
  *
- * WIP, do NOT use yet
- *
  */
 
 class BismuthNative {
 
-    private $version = '1.0.0';
-    private $server;
-    private $port;
-    private $verbose:
-    private $socket;
+	private $version = '1.0.0';
+	private $server;
+	private $port;
+	private $verbose;
+	private $socket;
 
-    public function __construct($server='127.0.0.1', $port=5658, $verbose=false) {
-        $this->server = $server;
-        $this->port = $port;
-        $this->verbose = $verbose
-        $this->socket = false;
-        $this->check_connection();
-    }
+	public function __construct($server='127.0.0.1', $port=5658, $verbose=false) {
+		$this->server = $server;
+		$this->port = $port;
+		$this->verbose = $verbose;
+		$this->socket = false;
+		$this->check_connection();
+	}
 
-    protected function check_connection(){
-        // Check connection state and reconnect if needed.
-        if (!$this->socket) {
-            try {
-                if ($this->verbose) {
-                    print('Connecting to '.$this->server.':'.$this->port."\n");
-                }
-                $this->socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-                socket_connect($this->socket, $this->server, $this->port);
-            } catch (Exception $e) {
-                $this->socket = false;
-                print("Connections: Error ".$e->getMessage());
-            }
-        }
-    }
+	protected function check_connection(){
+		// Check connection state and reconnect if needed.
+		if (!$this->socket) {
+			try {
+				if ($this->verbose) {
+					print('Connecting to '.$this->server.':'.$this->port."\n");
+				}
+				$this->socket = socket_create(AF_INET, SOCK_STREAM, 0);
+				//socket_set_block($this->socket);
+				socket_set_option($this->socket, SOL_SOCKET, SO_SNDTIMEO, array("sec"=>10, "usec"=>0));
+				socket_connect($this->socket, $this->server, $this->port);
+			} catch (Exception $e) {
+				$this->socket = false;
+				print("Connections: Error ".$e->getMessage());
+			}
+		}
+	}
+	
+
+
+	protected function _send($data, $retry=true) {
+		/* Sends something to the server*/
+		$this->check_connection();
+		try {
+			# Make sure the packet is sent in one call
+			$sdata = json_encode($data);
+			$header = str_pad(strlen($sdata), 10, '0', STR_PAD_LEFT);
+			$all = $header.$sdata;
+			$res = socket_write($this->socket, $all, 10+strlen($sdata));
+			if ($this->verbose) {
+				print("sent ".$all."\n");
+			}
+			return(true);
+		} catch (Exception $e) {
+			# send failed, try to reconnect
+			# TODO: handle tries #
+			$this->socket = false;
+			if ($retry) {
+				if ($this->verbose) {
+					print("Send failed (".$e->getMessage()."), trying to reconnect.");
+				}
+				$this->check_connection();
+			} else {
+				if ($this->verbose) {
+					print("Send failed (".$e->getMessage()."), not retrying.");
+				}
+				return(false);
+			}
+			try {
+				$header = str_pad(len($sdata), 10, '0', STR_PAD_LEFT);
+				$res = socket_write($this->socket, $header+$sdata, 10+len($sdata));
+				return(true);
+			} catch (Exception $e) {
+				$this->socket = false;
+				print("Send failed (".$e->getMessage().").");
+			}
+		 }
+	}
+
+	protected function  _receive() {
+		/* Wait for an answer, for LTIMEOUT sec.*/
+		$this->check_connection();
+		$data = '';
+		try {
+			$got = socket_recv($this->socket, $data, 10, MSG_WAITALL); 
+			//print("Got ".$got);
+			if ($got < 10) {
+				throw new Exception("Socket EOF");
+			}
+		} catch (Exception $e) {
+			$this->socket = false; return("");
+		}
+		try {
+			$chunks = "";
+			$bytes_recd = 0;
+			while ($bytes_recd < $data) {
+				$chunk = '';
+				$got = socket_recv($this->socket, $chunk, min($data - $bytes_recd, 2048), MSG_WAITALL); 
+				if (!$got) {
+					throw new Exception("Socket EOF2");
+				}
+				$chunks .= $chunk;
+				$bytes_recd += strlen($chunk);
+			}
+			return(json_decode($chunks, true));
+		} catch (Exception $e) {
+			$this->socket = false;
+			throw new Exception("Receive: ".$e->getMessage());
+		}
+	}
+	
+	public function command($command, $options=False) {
+		/*
+		Sends a command and return it's raw result.
+		options has to be a list.
+		Each item of options will be sent separately. So If you ant to send a list, pass a list of list.
+		*/
+		try {
+			$ret = false;
+			$this->_send($command);
+			if ($options) {
+				foreach ($options as $option) {
+					$this->_send($option, false);
+				}
+			}
+			$ret = $this->_receive();
+		} catch (Exception $e) {
+			// TODO : better handling of tries and delay between
+			if ($this->self.verbose) {
+				print('Error '.$e->getMessage().' sending command, trying to reconnect.');
+			}
+			$this->check_connection();
+			$this->_send($command);
+			if ($options) {
+				foreach ($options as $option) {
+					$this->_send($option, false);
+				}
+			}
+			$ret = $this->_receive();
+		}
+		return($ret);
+	}
+
+	protected function close() {
+		//Close the socket
+		try {
+			socket_close($this->socket);
+		} catch(Exception $e) {}
+   }
 
 }
--- PHP/demo.php
+++ PHP/demo.php
@@ -18,45 +18,45 @@ $SERVER = '127.0.0.1';
 // Default port
 $PORT = 5658;
 
-$VERBOSE = true;
+$VERBOSE = false;
 
 $connection = new BismuthNative($SERVER, $PORT, $VERBOSE);
 
 // Ask for general status
-$ret = $connection.command('statusjson');
+$ret = $connection->command('statusjson');
 print('statusjson:');
-print_r(ret$);
+print_r($ret);
 print('---------------------------------------');
 // Gets a specific block details
 // With this sample client library, params are encoded as a list even if there is only one param
-$ret = $connection.command('blockget', [558742]);
+$ret = $connection->command('blockget', [558742]);
 print('blockget(558742):');
 print_r($ret);
 print('---------------------------------------');
 // Gets latest block and diff
-$ret = $connection.command('difflast');
+$ret = $connection->command('difflast');
 print('difflast:');
 print_r($ret);
 print('---------------------------------------');
 // Gets tx detail, raw format
-$ret = $connection.command('api_gettransaction', ['K1iuKwkOac4HSuzEBDxmqb5dOmfXEK98BaWQFHltdrbDd0C5iIEbh/Fj', False]);
+$ret = $connection->command('api_gettransaction', ['K1iuKwkOac4HSuzEBDxmqb5dOmfXEK98BaWQFHltdrbDd0C5iIEbh/Fj', False]);
 print("api_gettransaction('K1iuKwkOac4HSuzEBDxmqb5dOmfXEK98BaWQFHltdrbDd0C5iIEbh/Fj', False):");
 print_r($ret);
 print('---------------------------------------');
 // Gets tx detail, json format
-$ret = $connection.command('api_gettransaction', ['K1iuKwkOac4HSuzEBDxmqb5dOmfXEK98BaWQFHltdrbDd0C5iIEbh/Fj', True]);
+$ret = $connection->command('api_gettransaction', ['K1iuKwkOac4HSuzEBDxmqb5dOmfXEK98BaWQFHltdrbDd0C5iIEbh/Fj', True]);
 print("api_gettransaction('K1iuKwkOac4HSuzEBDxmqb5dOmfXEK98BaWQFHltdrbDd0C5iIEbh/Fj', True):");
 print_r($ret);
 print('---------------------------------------');
 // Gets addresses balances
 // Since the param is a list, we send a list of list.
-$ret = $connection.command('api_listbalance', [ ['731337bb0f76463d578626a48367dfea4c6efcfa317604814f875d10','340c195f768be515488a6efedb958e135150b2ef3e53573a7017ac7d'], 0, True]);
+$ret = $connection->command('api_listbalance', [ ['731337bb0f76463d578626a48367dfea4c6efcfa317604814f875d10','340c195f768be515488a6efedb958e135150b2ef3e53573a7017ac7d'], 0, True]);
 print("api_listbalance(['731337bb0f76463d578626a48367dfea4c6efcfa317604814f875d10','340c195f768be515488a6efedb958e135150b2ef3e53573a7017ac7d'], 0, True):");
-print_r(ret);
+print_r($ret);
 print('---------------------------------------');
 // Ask for a new keys/address set
-$ret = $connection.command('keygen');
+$ret = $connection->command('keygen');
 print('keygen:');
 print_r($ret);
-print('---------------------------------------')
+print('---------------------------------------');
 
